825c36934f326966056435c6c40a9d6c8c4e162a
Eduardo Andrade <eaf.qafull@gmail.com>
2019-10-23 16:03:06 -0300

    TEste

--- AspNetCore.Web/Program.cs
+++ AspNetCore.Web/Program.cs
@@ -20,5 +20,8 @@ namespace AspNetCore.Web
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        public void teste()
+        { }
     }
 }
